3c7700a9553c58de1ce779ef4cd6154b769026d4
u <mjkim2304@gmail.com>
2022-12-23 14:42:28 +0900

    인증, 인가 기능 추가

---
+++ src/main/java/com/example/finalproject/config/JwtFilter.java
@@ -0,0 +1,66 @@
+package com.example.finalproject.config;
+
+import com.example.finalproject.service.UserService;
+import com.example.finalproject.utils.JwtTokenUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+// token이 있는지 매번 체크
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final UserService userService;
+    private final String secretKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        final String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+        log.info("authorization : {}", authorization);
+
+
+        // token 안보내면 block
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+            log.error("Authorizaiton을 잘못 보냈습니다.");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // authorization에서 token 꺼내기
+        String token = authorization.split("")[1];
+
+        // token이 expire됐는지 확인
+        if (JwtTokenUtil.isExpired(token, secretKey)) {
+            log.error("token이 만료 되었습니다");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // username 토큰에서 꺼내기
+        String username = JwtTokenUtil.getUsername(token, secretKey);
+        log.info("username:{}", username);
+
+        //권한 부여
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(username, null, List.of(new SimpleGrantedAuthority("USER")));
+
+        // detail들을 넣어준다
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/com/example/finalproject/utils/JwtTokenUtil.java
+++ src/main/java/com/example/finalproject/utils/JwtTokenUtil.java
@@ -7,6 +7,18 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
 
 public class JwtTokenUtil {
+
+    public static String getUsername(String token, String secretKey) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token)
+                .getBody().get("username", String.class);
+    }
+
+    public static boolean isExpired(String token, String secretKey) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token)
+                .getBody().getExpiration().before(new Date());
+
+    }
+
     public static String createToken(String username, String key, long expireTimeMs) {
         Claims claims = Jwts.claims();
         claims.put("username", username);
