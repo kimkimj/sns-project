cee81c0b250599e2b3f5e35b4cb351e603250008
u <mjkim2304@gmail.com>
2023-01-03 10:19:18 +0900

    UserControllerTest

--- src/test/java/com/example/finalproject/controller/UserControllerTest.java
+++ src/test/java/com/example/finalproject/controller/UserControllerTest.java
@@ -1,16 +1,26 @@
 package com.example.finalproject.controller;
 
+import com.example.finalproject.domain.dto.user.UserDto;
+import com.example.finalproject.domain.dto.user.UserJoinRequest;
+import com.example.finalproject.exception.AppException;
+import com.example.finalproject.exception.ErrorCode;
 import com.example.finalproject.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 class UserControllerTest {
@@ -24,5 +34,38 @@ class UserControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @Test
+    @DisplayName("회원 가입 성공")
+    void join_success() throws Exception {
+        UserJoinRequest userJoinRequest = UserJoinRequest.builder()
+                .userName("orange")
+                .password("1234")
+                .build();
+        when(userService.join(any())).thenReturn(mock(UserDto.class));
+        mockMvc.perform(post("/api/v1/users/join")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("회원가입 실패")
+    void join_fail() throws Exception {
+        UserJoinRequest userJoinRequest = UserJoinRequest.builder()
+                .userName("orange")
+                .password("1234")
+                .build();
+
+        when(userService.join(any())).thenThrow(new AppException(ErrorCode.USERNAME_DUPLICATED, ""));
+
+        mockMvc.perform(post("/api/v1/users/join")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
+                .andDo(print())
+                .andExpect(status().isConflict());
+    }
 
 }
