663a7c5cf161a624c1b72959b57c3904b1070f98
u <mjkim2304@gmail.com>
2023-09-10 15:08:59 +0900

    REFACTOR: Alarm - @Transactional 사용

--- src/main/java/com/example/finalproject/service/AlarmService.java
+++ src/main/java/com/example/finalproject/service/AlarmService.java
@@ -1,7 +1,6 @@
 package com.example.finalproject.service;
 
 import com.example.finalproject.domain.dto.alarm.AlarmListResponse;
-import com.example.finalproject.domain.dto.alarm.AlarmResponse;
 import com.example.finalproject.domain.entity.Alarm;
 import com.example.finalproject.domain.entity.User;
 import com.example.finalproject.exception.AppException;
@@ -14,11 +13,13 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class AlarmService {
     private final AlarmRepository alarmRepository;
     private final UserRepository userRepository;
